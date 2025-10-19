eea48d4153ac2b87a4d2cc43c10425878314d5bc
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2025-10-19 13:25:54 +0100

    Use environment variable for API base URL

--- src/app/types/auth.ts
+++ src/app/types/auth.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE;;
 
 export async function request(path: string, options: RequestInit) {
   const res = await fetch(`${API_BASE}${path}`, {
