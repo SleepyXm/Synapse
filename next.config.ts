51cf51ff74f8f10cfc5446c7bf132915dae45620
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-03-01 00:04:36 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE}/:path*`,
       },
     ];
   },
