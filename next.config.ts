25eaefc8908c51befc5d66e65e60ba7a3b19a3f2
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-03-17 02:59:51 +0000

    Remove empty rewrites function
    Removed empty rewrites function from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cdn-avatars.huggingface.co"],
   },
-  async rewrites() {
-    return [
-      {
-      }
-    ];
-  },
 };
 
 export default nextConfig;
